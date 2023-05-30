243f3230a1047a6112728d85a40295fa461b5ecd
Titicc <83614737+Titicc@users.noreply.github.com>
2023-05-30 11:23:05 -0600

    modelos

--- ProyectoVideoteca_B92291_B91334/Models/Episodes.cs
+++ ProyectoVideoteca_B92291_B91334/Models/Episodes.cs
@@ -2,5 +2,14 @@
 {
     public class Episodes
     {
+        public int episode_id { get; set;}
+
+        public string tittle { get; set;}
+
+        public string duration { get; set;}
+
+        public int episode_number { get; set;}
+
+        public int movies_series_id { get; set;}
     }
 }
--- ProyectoVideoteca_B92291_B91334/Models/MoviesAndSeries.cs
+++ ProyectoVideoteca_B92291_B91334/Models/MoviesAndSeries.cs
@@ -2,5 +2,27 @@
 {
     public class MoviesAndSeries
     {
+        public int id { get; set; }
+        public string title { get; set; }
+
+        public string synopsis { get; set; }
+
+        public int release_year { get; set; }
+
+        public string duration { get; set; }
+
+        public string director { get; set; }
+
+        public string genre_id { get; set; }
+
+        public string num_seasons { get; set; }
+
+        public string num_episodes { get; set; }
+
+        public string episode_duration { get; set; }
+
+        public string movie_cover { get; set; }
+
+        public string data_added { get; set; }
     }
 }

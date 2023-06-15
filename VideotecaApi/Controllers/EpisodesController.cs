bf48be54b651efcbba583c1878c0e252545fe789
83gerson <gersoncalvo83@gmail.com>
2023-06-15 12:27:16 -0600

    Api Episodes
    Implementación de todos los métodos del EpisodesController.

--- VideotecaApi/Controllers/EpisodesController.cs
+++ VideotecaApi/Controllers/EpisodesController.cs
@@ -9,17 +9,70 @@ namespace VideotecaApi.Controllers
     [ApiController]
     public class EpisodesController : ControllerBase
     {
-        private readonly UCRTextContext _context;
+        private readonly UCRTextContext _context = new UCRTextContext();
 
-        public EpisodesController(UCRTextContext context)
-        {
-            _context = context;
-        }
-
+        // GET api/actor/episodes
         [HttpGet("episodes")]
         public IActionResult GetEpisodes()
         {
-            return Ok();
+            var episodes = _context.Episodes.ToList();
+            return Ok(episodes);
+        }
+
+        // GET api/episodes/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetEpisodes(int id)
+        {
+            var episodes = _context.Episodes.Find(id);
+            if (episodes == null)
+            {
+                return NotFound();
+            }
+            return Ok(episodes);
+        }
+
+        // POST api/episodes
+        [HttpPost]
+        public IActionResult CreateActor([FromBody] Episodes episode)
+        {
+            _context.Episodes.Add(episode);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetEpisodes), new { id = episode.episode_id }, episode);
+        }
+
+        // PUT api/episodes/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateEpisodes(int id, [FromBody] Episodes episodeData)
+        {
+            var episode = _context.Episodes.Find(id);
+            if (episode == null)
+            {
+                return NotFound();
+            }
+
+            episode.tittle = episodeData.tittle;
+            episode.duration = episodeData.duration;
+            episode.episode_number = episodeData.episode_number;
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE api/episodes/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEpisodes(int id)
+        {
+            var episode = _context.Episodes.Find(id);
+            if (episode == null)
+            {
+                return NotFound();
+            }
+
+            _context.Episodes.Remove(episode);
+            _context.SaveChanges();
+
+            return NoContent();
         }
 
     }
